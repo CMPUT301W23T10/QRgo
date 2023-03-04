e30cf5a385b9e6025aa942b10cfee30eff8d2481
Taylor <tcurran@ualberta.ca>
2023-03-03 22:03:45 -0700

    Created signup layout

---
+++ app/src/main/java/com/example/qrgo/utilities/SignupActivity.java
@@ -0,0 +1,16 @@
+package com.example.qrgo.utilities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.qrgo.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
