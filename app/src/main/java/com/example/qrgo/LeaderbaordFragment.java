888755f6e70de13f2e58d17e81d4166bfa58a1ae
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-03-12 17:50:19 -0600

    bug fix
    bug fix

--- app/src/main/java/com/example/qrgo/LeaderbaordFragment.java
+++ app/src/main/java/com/example/qrgo/LeaderbaordFragment.java
@@ -41,37 +41,11 @@ public class LeaderbaordFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LeaderbaordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LeaderbaordFragment newInstance(String param1, String param2) {
-        LeaderbaordFragment fragment = new LeaderbaordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        Log.d("LeaderboardFragment", "onCreateView: ");
         View rootView = inflater.inflate(R.layout.fragment_leaderboard, container, false);
 
         leaderboardList = rootView.findViewById(R.id.all_users_list_view);
@@ -108,6 +82,6 @@ public class LeaderbaordFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaderbaord_blank, container, false);
+        return inflater.inflate(R.layout.fragment_leaderboard, container, false);
     }
 }
