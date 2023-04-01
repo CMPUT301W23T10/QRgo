aebc02fd955b812fa15d963553ed7d5ecf83564c
Fardeen Faisal <68156295+fardeenfs@users.noreply.github.com>
2023-04-01 15:22:15 -0600

    Tests Commit

--- app/src/androidTest/java/com/example/qrgo/IntentTests/A_UserSignupTests.java
+++ app/src/androidTest/java/com/example/qrgo/IntentTests/A_UserSignupTests.java
@@ -11,10 +11,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 
 import android.util.Log;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
@@ -24,10 +21,7 @@ import androidx.test.runner.AndroidJUnitRunner;
 import com.example.qrgo.HomeActivity;
 import com.example.qrgo.MainActivity;
 import com.example.qrgo.PlayerActivity;
-import com.example.qrgo.QRIntakeActivity;
-import com.example.qrgo.QrProfileActivity;
 import com.example.qrgo.R;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.robotium.solo.Solo;
 
@@ -38,11 +32,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.List;
-
 
 @RunWith(AndroidJUnit4.class)
-public class A_CreateNewUserAddQrTest extends AndroidJUnitRunner {
+public class A_UserSignupTests extends AndroidJUnitRunner {
 
     private static Solo solo;
     private static String username;
@@ -83,10 +75,10 @@ public class A_CreateNewUserAddQrTest extends AndroidJUnitRunner {
     public void testSignUpUser() {
         // Launch the MainActivity
         solo.assertCurrentActivity("Expected MainActivity", MainActivity.class);
-        solo.enterText((EditText) solo.getView(R.id.username), "first");
-        solo.enterText((EditText) solo.getView(R.id.address), "last");
-        solo.enterText((EditText) solo.getView(R.id.phone_number), "testemail@gmail.com");
-        solo.enterText((EditText) solo.getView(R.id.phone_number2), "1233456");
+        solo.enterText((EditText) solo.getView(R.id.firstNameEntry), "first");
+        solo.enterText((EditText) solo.getView(R.id.lastNameEntry), "last");
+        solo.enterText((EditText) solo.getView(R.id.emailEntry), "testemail@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.phoneEntry), "1234567890");
         solo.clickOnView(solo.getView(R.id.register));
 
         // Wait for home activity to launch
--- app/src/main/java/com/example/qrgo/SignupActivity.java
+++ app/src/main/java/com/example/qrgo/SignupActivity.java
@@ -139,7 +139,7 @@ public class SignupActivity extends AppCompatActivity {
                     @Override
                     public void onUserAdd(boolean success) {
                         //Add Profile
-                        db.getPlayerProfileManager().addNewPlayerProfile(userName, firstName, lastName, contactEmail, contactPhone, 0, 0, 0, 0, new OnUserProfileAddListener() {
+                        db.getPlayerProfileManager().addNewPlayerProfile(userName, firstName, lastName,  contactPhone, contactEmail,0, 0, 0, 0, new OnUserProfileAddListener() {
                             @Override
 
                             public void onUserProfileAdd(boolean success) {
