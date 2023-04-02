550fec4da6b906f5226459c98b0407cbf5a1ffbf
Akarshan Mishra <pharaohsupreme03@gmail.com>
2023-04-02 13:55:38 -0600

    fixed comment UI issue, removed boiler plate code

--- app/src/main/java/com/example/qrgo/CommentListviewFragment.java
+++ app/src/main/java/com/example/qrgo/CommentListviewFragment.java
@@ -42,36 +42,6 @@ public class CommentListviewFragment extends Fragment {
         this.commentList = commentList;
 
     }
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment QrListview.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CommentListviewFragment newInstance(String param1, String param2) {
-
-        CommentListviewFragment fragment = new CommentListviewFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
